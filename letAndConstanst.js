5e365bdba137358f5dd51a403c12d6a49edea42c
jadsonmarliere <jds.mdo@gmail.com>
2020-03-19 18:13:19 -0400

    Update letAndConstanst.js

--- letAndConstanst.js
+++ letAndConstanst.js
@@ -1,21 +1,21 @@
 function main() {
 
-    const r = 0
-    let PI = 2.6
+    let area = 0;
+    let perimeter = 0;
+    var r = readLine();
+    const PI = Math.PI;
 
-    readLine(r)
+    area = PI * Math.pow(r, 2);
+    perimeter = 2 * PI * r;
 
-    area = PI * Math.pow(r, 2)
-    perimeter = 2 * PI * r
+    console.log(area);
+    console.log(perimeter);
 
-    console.log(area)
-    console.log(perimeter)
+    try {
 
-    try {    
-        // Attempt to redefine the value of constant variable PI
         PI = 0;
-        // Attempt to print the value of PI
         console.log(PI);
+
     } catch(error) {
         console.error("You correctly declared 'PI' as a constant.");
     }
