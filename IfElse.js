06f49c7593764ae0dee528cf461cfd5b2522ab10
jadsonmarliere <jds.mdo@gmail.com>
2020-03-20 23:06:11 -0400

    ifElse

---
+++ IfElse.js
@@ -0,0 +1,34 @@
+
+/*
+// Others ways to solve
+
+function getGrade(score) {
+    return 'FEDCBA'[parseInt((score > 0 ? score - 1 : 0) / 5)];
+}
+
+*/
+------------------------------------------
+
+var score = 20;
+
+function getGrade(score) {
+    
+    if( 0 <= score <= 30){
+        if(score <=5){
+			return 'F';
+		}else if(score <= 10){
+			return 'E';
+		}else if(score <= 15){
+			return 'D';
+		}else if(score <= 20){
+			return 'C';
+		}else if(score <= 25){
+			return 'B';
+		}else 
+			return 'A';
+    }else{
+        console.log('Digite um valor entre 0 e 30');
+		return 0
+    }	
+}
+
