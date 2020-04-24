8522fa104455ce28a1f7f4a1b9038b456ca7808e
jadsonmarliere <jds.mdo@gmail.com>
2020-04-24 12:43:25 -0300

    create contatcForm.js

---
+++ js/contatcForm.js
@@ -0,0 +1,10 @@
+
+function Send() {
+ 
+    var nome = document.getElementById("nameId");
+ 
+    if (name.value != "") {
+        alert('Thank you Mr/Ms) ' + name.value + ' you sent your data with sucess!');
+    }
+ 
+}
