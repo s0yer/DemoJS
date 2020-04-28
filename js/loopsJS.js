3caac772a81917cd3e2b56c3f870519f13fbf4f9
jadsonmarliere <jds.mdo@gmail.com>
2020-04-28 01:25:30 -0300

    update loopsJS.js

--- js/loopsJS.js
+++ js/loopsJS.js
@@ -1,38 +1,6 @@
 
-/*
- //Others ways to solve
 
-function vowelsAndConsonants(s) {
-    const vowels = 'aeiou';
-    var consonants = '';
-    
-    for(var i = 0; i < s.length; i++) {
-       if (vowels.includes(s[i])) {
-           console.log(s[i]);
-       }
-       else {
-           consonants += s[i] + '\n';
-       }
-    }
-    
-    console.log(consonants.trim());
-}
-
-------------------------------------------
-
-function vowelsAndConsonants(s) {
-    [...s].forEach(c => 'aeiou'.includes(c) ?
-        console.log(c) : null);
-    [...s].forEach(c => 'aeiou'.includes(c) ?
-        null : console.log(c));
-}
-
---------------------------------------------
-
-*/
-
-
-const word = 'easypeasy'
+const word = 'easypeasy';
 
 function vowelsAndConsonants(s) {
     let vowels = [ 'a', 'e', 'i', 'o', 'u'];
@@ -40,15 +8,18 @@ function vowelsAndConsonants(s) {
 	// print the vowels first
 	for(let v of s){
 		if(vowels.includes(v)){
-			console.log(v);
+			document.write(v);
 		}
 	}
 	
 	// print the consonants second
 	for(let v of s){
 		if(!vowels.includes(v)){
-			console.log(v);
+			document.write(v);
 		}
 	}
 }
 
+document.write("<br>","Vowel and Consonant Function: ","<br>","<br>");
+vowelsAndConsonants(word);
+document.write("<br>","--------------------------------------------------","<br>","<br>");
