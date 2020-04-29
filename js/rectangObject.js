070b6a9bea824120e3df9c5330777a53c008c5ca
jadsonmarliere <jds.mdo@gmail.com>
2020-04-29 09:45:12 -0300

    Update rectangObject.js

--- js/rectangObject.js
+++ js/rectangObject.js
@@ -1,8 +1,19 @@
 
 
-function Rectangle(a, b) {
+function rectangle(a, b) {
     this.length = a;
     this.width = b;
     this.perimeter = 2 * (a+b);
     this.area = a * b;
-}
+    
+    document.write("Perimeter = " + this.perimeter);
+    document.write("Area = " + this.area);
+}   
+
+const x = 21;
+const y = 34;
+
+
+document.write("<br>","Retangle Function: ","<br>","<br>");
+rectangle(x,y)
+document.write("<br>","--------------------------------------------------","<br>","<br>");
