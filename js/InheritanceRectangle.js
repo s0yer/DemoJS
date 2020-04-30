efaf75008df3f7ece5f8969c5e092e82a2f77181
jadsonmarliere <jds.mdo@gmail.com>
2020-04-30 16:25:17 -0300

    update inheritanceRectangle.js

--- js/InheritanceRectangle.js
+++ js/InheritanceRectangle.js
@@ -24,6 +24,8 @@ class Square extends Rectangle{
 
 const rec = new Rectangle(3, 4);
 const sqr = new Square(3);
-    
-console.log(rec.area());
-console.log(sqr.area());
+
+document.write("<br>","Objects and Inheritance: ","<br>","<br>");
+document.write(rec.area());
+document.write(sqr.area());
+document.write("<br>","--------------------------------------------------","<br>","<br>");
