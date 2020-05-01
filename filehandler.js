f5b977a16dcd1d24e866990a59eef9a94f7ff66e
jadsonmarliere <jds.mdo@gmail.com>
2020-05-01 11:00:01 -0300

    Update filehandler.js

--- filehandler.js
+++ filehandler.js
@@ -1,1 +1,13 @@
-//filehandler.js
+
+
+var fs = require('fs');
+
+module.exports = function(filename, successFn, errorFn) {
+	fs.readFile(filename, function(err, data) {
+		if(err) {
+			errorFn(err);
+		} else {
+			successFn(data);
+		}
+	});
+};
