12d7b8cdd4a844986264939243c2baaefcd8e57e
jadsonmarliere <jds.mdo@gmail.com>
2020-05-05 21:43:08 -0300

    update functions

--- static/js/InheritanceRectangle.js
+++ static/js/InheritanceRectangle.js
@@ -3,14 +3,14 @@ class Rectangle {
         this.w = w;
         this.h = h;
     }
+    
+     // method to the Rectangle class' prototype
+    Rectangle.area = function(){
+        return (this.w*this.h);
+    }
+    
 }
 
-
- // method to the Rectangle class' prototype
-
-Rectangle.prototype.area = function(){
-    return(this.w*this.h);
-}
 
  // Square class that inherits from Rectangle and implement its //class constructor
 
@@ -22,8 +22,8 @@ class Square extends Rectangle{
     }
 }
 
-const rec = new Rectangle(3, 4);
-const sqr = new Square(3);
+rec = new Rectangle(3, 4);
+sqr = new Square(3);
 
 document.write("<br>","Objects and Inheritance: ","<br>","<br>");
 document.write(rec.area());
--- static/js/regexIII.js
+++ static/js/regexIII.js
@@ -5,18 +5,20 @@ function regexVar() {
         Other Way:
         return new RegExp('\\d+', 'g');
 
+        let re = /\d+/g;
+        return re;
      */
-    
-    let re = /\d+/g;
-    return re;
+  let re = new RegExp('\\d+', 'g');
+   return re;
 }
 
 
 
 const re = regexVar();
 const s = "1.1235813";
-
 const r = s.match(re);
+
+//document.write(!!s.match(re));
 
 document.write("<br>","Regex II : ","<br>","<br>");
 for (const e of r) {
