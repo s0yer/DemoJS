00af3401dc0d724cbf777f1426007d9b47789d1a
jadsonmarliere <jds.mdo@gmail.com>
2020-05-07 17:06:41 -0300

    update regexIII.js

--- static/js/regexIII.js
+++ static/js/regexIII.js
@@ -3,25 +3,24 @@ function regexVar() {
 
  /*
         Other Way:
-        return new RegExp('\\d+', 'g');
-
-        let re = /\d+/g;
-        return re;
+        let re = new RegExp('\\d+', 'g');
+        
      */
-  let re = new RegExp('\\d+', 'g');
-   return re;
+    let re = /\d+/g;
+    return re;
 }
 
 
 
-const re = regexVar();
-const s = "1.1235813";
-const r = s.match(re);
+const rex = regexVar();
+const sst = "1.1235813";
+var r = sst.match(rex);
 
-//document.write(!!s.match(re));
+
 
 document.write("<br>","Regex II : ","<br>","<br>");
+document.write(!!sst.match(rex),"<br>");
 for (const e of r) {
-    document.write(e);
+   document.write(e);
 }
 document.write("<br>","--------------------------------------------------","<br>","<br>");
