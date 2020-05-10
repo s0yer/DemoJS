51a3010541a33553de3a8118e09469883d1a3a23
jadsonmarliere <jds.mdo@gmail.com>
2020-05-10 20:41:59 -0300

    update templateLiterals.js

--- static/js/templateLiterals.js
+++ static/js/templateLiterals.js
@@ -22,6 +22,6 @@ function main() {
     
     const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
     
-    console.log((s1 === x) ? s1 : -1);
-    console.log((s2 === y) ? s2 : -1);
+   document.write((s1 === x) ? s1 : -1);
+   document.write((s2 === y) ? s2 : -1);
 }
